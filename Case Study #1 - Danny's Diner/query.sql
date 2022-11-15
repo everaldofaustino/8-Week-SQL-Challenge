1578d417aab5a3be6d0847b500118b480f6e4355
everaldofaustino <everaldo_faustino@yahoo.co.br>
2022-11-15 17:44:56 -0300

    case_study_1

--- Case Study #1 - Danny's Diner/query.sql
+++ Case Study #1 - Danny's Diner/query.sql
@@ -1,3 +1,72 @@
+# Case Study #1 - Danny's Diner
+
+
+
+/* --------------------
+   Case Study Questions
+   --------------------*/
+
+-- 1. What is the total amount each customer spent at the restaurant?
+     
+    
+ **Query #1**
+
+    SELECT
+       sales.customer_id,
+        sum(menu.price)
+        
+    FROM dannys_diner.sales
+    inner join dannys_diner.menu on sales.product_id = menu.product_id
+    group by sales.customer_id
+    ORDER BY sum(menu.price) desc;
+
+
+**Query #2**
+
+-- 2. How many days has each customer visited the restaurant?
+
+    SELECT
+       customer_id,
+        count(distinct order_date) as Count_days
+        
+    FROM dannys_diner.sales
+    group by sales.customer_id;
+
+| customer_id | count_days |
+| ----------- | ---------- |
+| A           | 4          |
+| B           | 6          |
+| C           | 2          |
+
+**-- 3. What was the first item from the menu purchased by each customer?**
+
+    WITH partitioned AS (
+        SELECT customer_id,
+               order_date,
+               product_id
+              ,DENSE_RANK() OVER(PARTITION BY customer_id
+                                ORDER BY order_date) AS rank
+        FROM dannys_diner.sales
+    )
+    SELECT partitioned.customer_id,
+              partitioned.order_date,
+           menu.product_name
+    FROM partitioned
+    INNER JOIN dannys_diner.menu ON partitioned.product_id = menu.product_id
+    where partitioned.rank = 1
+    group by partitioned.customer_id,
+              partitioned.order_date,
+              menu.product_name;
+
+| customer_id | order_date               | product_name |
+| ----------- | ------------------------ | ------------ |
+| A           | 2021-01-01T00:00:00.000Z | curry        |
+| A           | 2021-01-01T00:00:00.000Z | sushi        |
+| B           | 2021-01-01T00:00:00.000Z | curry        |
+| C           | 2021-01-01T00:00:00.000Z | ramen        |
+
+---
+
 
 
 **Query #4**
