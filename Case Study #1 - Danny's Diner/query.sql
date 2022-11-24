193911eaea3abfa37fedcdf4acffdede466df4e3
Everaldo Faustino <43473230+everaldofaustino@users.noreply.github.com>
2022-11-24 10:39:08 -0300

    Update query.sql

--- Case Study #1 - Danny's Diner/query.sql
+++ Case Study #1 - Danny's Diner/query.sql
@@ -137,4 +137,86 @@
 | B           | sushi        | 1       |
 
 ---
+**Schema (PostgreSQL v13)**
+
+    CREATE SCHEMA dannys_diner;
+    SET search_path = dannys_diner;
+    
+    CREATE TABLE sales (
+      "customer_id" VARCHAR(1),
+      "order_date" DATE,
+      "product_id" INTEGER
+    );
+    
+    INSERT INTO sales
+      ("customer_id", "order_date", "product_id")
+    VALUES
+      ('A', '2021-01-01', '1'),
+      ('A', '2021-01-01', '2'),
+      ('A', '2021-01-07', '2'),
+      ('A', '2021-01-10', '3'),
+      ('A', '2021-01-11', '3'),
+      ('A', '2021-01-11', '3'),
+      ('B', '2021-01-01', '2'),
+      ('B', '2021-01-02', '2'),
+      ('B', '2021-01-04', '1'),
+      ('B', '2021-01-11', '1'),
+      ('B', '2021-01-16', '3'),
+      ('B', '2021-02-01', '3'),
+      ('C', '2021-01-01', '3'),
+      ('C', '2021-01-01', '3'),
+      ('C', '2021-01-07', '3');
+     
+    
+    CREATE TABLE menu (
+      "product_id" INTEGER,
+      "product_name" VARCHAR(5),
+      "price" INTEGER
+    );
+    
+    INSERT INTO menu
+      ("product_id", "product_name", "price")
+    VALUES
+      ('1', 'sushi', '10'),
+      ('2', 'curry', '15'),
+      ('3', 'ramen', '12');
+      
+    
+    CREATE TABLE members (
+      "customer_id" VARCHAR(1),
+      "join_date" DATE
+    );
+    
+    INSERT INTO members
+      ("customer_id", "join_date")
+    VALUES
+      ('A', '2021-01-07'),
+      ('B', '2021-01-09');
+
+---
+
+**7. Which item was purchased just before the customer became a member?**
+
+    WITH partitioned AS (SELECT sales.customer_id,
+                       menu.product_name,
+                       DENSE_RANK() OVER(PARTITION BY sales.customer_id
+                                        ORDER BY order_date) AS ranking
+                      
+                FROM dannys_diner.sales
+                INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+                INNER JOIN dannys_diner.members ON sales.customer_id = members.customer_id
+                WHERE sales.order_date < members.join_date                     
+                
+                ) 
+        select * from partitioned
+        where partitioned.ranking = 1;
+
+| customer_id | product_name | ranking |
+| ----------- | ------------ | ------- |
+| A           | sushi        | 1       |
+| A           | curry        | 1       |
+| B           | curry        | 1       |
+
+---
+
 
